adccae0810ec927fc20291f01eb98baaa371d4e5
Patrick Kalkman <patrick@simpletechture.nl>
2022-01-21 17:05:19 +0100

    improved scripts

--- Add-AccessRestrictions.ps1
+++ Add-AccessRestrictions.ps1
@@ -1,22 +1,20 @@
-# Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
-
 <#
   .SYNOPSIS
-  Set the access restriction of all app services in the given resource group to the rules in the 
-  given JSON file.
+  Get the ip address from the ip-addresses.json and give these access to all the 
+  app services in the given resource group.  
 
   .DESCRIPTION
-  Set the access restriction of all app services in the given resource group to the rules in the 
-  given JSON file.
-
-  .PARAMETER ResourceGroupName
-  The resource group that contains the API management service and the app services to add the access restrictions to
+  Get the ip address from the ip-addresses.json and give these access to all the 
+  app services in the given resource group.
 
   .PARAMETER JsonFilename
-  The name of the JSON file that contains the rules and ip addresses to add to the access restriction
+  The file name of the json file that contains the ip addresses
+
+  .PARAMETER ResourceGroupName
+  The resource group that contains the app services to add the access restrictions to
 
   .PARAMETER RemoveExistingRules
-  Wether to remove all the existing access restrictions before adding the new ones
+  Wether to first remove all the existing access restrictions before adding the new ones
 
   .PARAMETER AppServicesToExclude
   A comma separated list of app services names that should be excluded
@@ -28,26 +26,29 @@
   None. Add-AccessRestrictions.ps1 does not generate any output.
 
   .EXAMPLE
-  PS> .\Add-AccessRestrictions.ps1 -ResourceGroupName resourceGroup
+  PS> .\Add-AccessRestrictions.ps1 -JsonFilename ./ip-addresses.json -ResourceGroupName MyResourceGroup
 
   .EXAMPLE
-  PS> .\Add-AccessRestrictions.ps1 -ResourceGroupName resourceGroup -RemoveExistingRules $True
+  PS> .\Add-AccessRestrictions.ps1 -JsonFilename ./ip-addresses.json -ResourceGroupName MyResourceGroup  -RemoveExistingRules $True
+
+  .EXAMPLE
+  PS> .\Add-AccessRestrictions.ps1 -JsonFilename ./ip-addresses.json -ResourceGroupName MyResourceGroup -RemoveExistingRules $True -AppServicesToExclude myAppServiceToExclude1, myAppServiceToExclude2
 
 #>
 Param(
   [Parameter(Mandatory=$True)]
-  [String] $ResourceGroupName,
-  [Parameter(Mandatory=$True)]
   [String] $JsonFilename,
+  [Parameter(Mandatory=$True)]
+  [String] $ResourceGroupName,
   [Parameter(Mandatory=$False)]
   [Boolean] $RemoveExistingRules = $False,
   [Parameter(Mandatory=$False)]
-  [String] $AppServicesToExclude
+  [String[]] $AppServicesToExclude = @()
 )
 
 Import-Module .\Manage-AccessRestrictions.psm1
 
 Write-Output "--> Starting access restriction script"
 LoginToAzure
-AddAccessRestrictionsToAppServices -ResourceGroupName $ResourceGroupName -JsonFilename $JsonFilename -RemoveExistingRules $RemoveExistingRules  -AppServicesToExclude $AppServicesToExclude
+Add-AccessRestrictionsToAppServices -JsonFilename $JsonFilename -ResourceGroupName $ResourceGroupName -RemoveExistingRules $RemoveExistingRules -AppServicesToExclude $AppServicesToExclude
 Write-Output "<-- Finished access restriction script"
--- Manage-AccessRestrictions.psm1
+++ Manage-AccessRestrictions.psm1
@@ -1,78 +1,123 @@
 function Add-AccessRestrictionsToAppServices([string] $ResourceGroupName, 
   [string] $JsonFilename, 
   [bool] $RemoveExistingRules, 
-  [string] $AppServicesToExclude)
+  [string[]] $AppServicesToExclude)
 {
+  Write-Information "Retrieving all app services in $ResourceGroupName"
+  $allAppServices = Get-AzWebApp -ResourceGroupName $ResourceGroupName
+
+  $confirmAppServicesToExclude = Confirm-AppServicesToExclude -AllAppServices $allAppServices -AppServicesToExclude $AppServicesToExclude
+  if (!$confirmAppServicesToExclude)
+  {
+    $errorMessage = "Did not find one or more of the app services in the exclude list, make sure that the names are spelled correctly" 
+    Write-Output $errorMessage 
+    throw $errorMessage
+  }
+  
   Write-Information "Reading the json file with ip rules"
   $ipRules = ReadJsonFile($JsonFilename)
 
-  Write-Information "Retrieving all app services in $ResourceGroupName"
-  $allAppServices = Get-AzWebApp -ResourceGroupName $ResourceGroupName
-  Foreach ($appService in $allAppServices)
+  :nextAppService Foreach ($appService in $allAppServices)
   {
     $appServiceName = $appService.Name
 
-    if($AppServicesToExclude.ToLower().Contains($appServiceName.ToLower()))
+    Foreach ($appServiceToExclude in $AppServicesToExclude)
     {
-      Write-Information "Skipped app service $appServiceName as it is in the exclude list"
-      continue
+      if($appServiceToExclude.ToLower().Contains($appServiceName.ToLower()))
+      {
+        Write-Output "Skipped app service $appServiceName as it is in the exclude list"
+        continue nextAppService
+      }
     }
 
     if ($RemoveExistingRules)
     {
-      $config = Get-AzWebAppAccessRestrictionConfig -ResourceGroupName $ResourceGroupName -Name $appService.Name
-      Write-Information "Removing existing access restrictions on $appServiceName"
-      Foreach ($accessRestriction in $config.MainSiteAccessRestrictions)
-      {
-        Write-Debug "Removing rule $accessRestriction.RuleName"
-        Remove-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $appService.Name -Name $accessRestriction.RuleName
-      }
+      Remove-AccessRestrictionsFromAppService -ResourceGroupName $ResourceGroupName -Name $appServiceName          
     }
 
     Foreach ($ipRule in $ipRules)
     {
-      $ruleName = $ipRule.rulename
-      $ipAddress = $ipRule.ipaddress
-      Write-Information "Adding rule $ruleName to allow ip $ipAddress access to $appServiceName"
-    
       # Construct ip address filter for access restriction /32 = single ip address
       $ipAddress = -join($ipRule.ipaddress, "/32");
-      Add-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $appService.Name -Name $ipRule.rulename -Priority 200 -Action Allow -IpAddress $ipAddress
-      # Set the ipfilter also on the scm site
-      Update-AzWebAppAccessRestrictionConfig -Name $appService.Name -ResourceGroupName $ResourceGroupName -ScmSiteUseMainSiteRestrictionConfig
+      Add-AccessRestrictionToAppService -ResourceGroupName $ResourceGroupName -Name $appServiceName  -IpFilterRuleName $ipRule.rulename -IpAddress $ipAddress -Priority $ipRule.priority
     }
   }
 }
 
-function Remove-AllAccessRestrictionsFromAppServices([string] $ResourceGroupName) {
+function Confirm-AppServicesToExclude([Object[]] $AllAppServices, [string[]] $AppServicesToExclude)
+{
+  if ($AppServicesToExclude.Length -gt 0)
+  {
+    Foreach ($appServiceToExclude in $AppServicesToExclude)
+    {
+      $appServiceExist = $AllAppServices | Where-Object { $_.Name -eq $appServiceToExclude }
+      if (!$appServiceExist)
+      {
+        Write-Information "Could not find $appServiceToExclude in the list of app services"
+        return $false
+      }
+    }
+  }
+  return $true
+}
+
+function Remove-AllAccessRestrictionsFromAppServices([string] $ResourceGroupName)
+{
   Write-Information "Retrieving all app services in $ResourceGroupName"
   $allAppServices = Get-AzWebApp -ResourceGroupName $ResourceGroupName
-  Foreach ($appService in $allAppServices) {
-      Remove-AccessRestrictionsFromAppService -ResourceGroupName $ResourceGroupName -Name $appService.Name
+  Foreach ($appService in $allAppServices)
+  {
+    Remove-AccessRestrictionsFromAppService -ResourceGroupName $ResourceGroupName -Name $appService.Name
   }
 }
 
-function Remove-AccessRestrictionsFromAppService([string] $ResourceGroupName, [string] $Name) {
+function Remove-AccessRestrictionsFromAppService([string] $ResourceGroupName, [string] $Name)
+{
   $config = Get-AzWebAppAccessRestrictionConfig -ResourceGroupName $ResourceGroupName -Name $Name
   Write-Information "Removing existing access restrictions on $Name"
-  Foreach ($accessRestriction in $config.MainSiteAccessRestrictions) {
-      $ruleName = $accessRestriction.RuleName
-      Write-Debug "Removing rule $ruleName"
-      Remove-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $Name -Name $accessRestriction.RuleName
+  Foreach ($accessRestriction in $config.MainSiteAccessRestrictions)
+  {
+    $ruleName = $accessRestriction.RuleName
+    Write-Debug "Removing rule $ruleName"
+    Remove-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $Name -Name $accessRestriction.RuleName
   }
 
   # Also remove all the access restriction from the staging slots
   $allSlots = Get-AzWebAppSlot -ResourceGroupName $ResourceGroupName -Name $Name
-  Foreach ($slot in $allSlots) {
-      $slotName = Get-SlotName -SlotName $slot.Name -AppServiceName $Name
-      Write-Information "Removing existing access restrictions on $Name slot $slotName"
-      $config = Get-AzWebAppAccessRestrictionConfig -ResourceGroupName $ResourceGroupName -Name $Name -SlotName $slotName
-      Foreach ($accessRestriction in $config.MainSiteAccessRestrictions) {
-          $ruleName = $accessRestriction.RuleName
-          Write-Debug "Removing rule $ruleName"
-          Remove-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $Name -Name $accessRestriction.RuleName -SlotName $slotName
-      }
+  Foreach ($slot in $allSlots)
+  {
+    $slotName = Get-SlotName -SlotName $slot.Name -AppServiceName $Name
+    Write-Information "Removing existing access restrictions on $Name slot $slotName"
+    $config = Get-AzWebAppAccessRestrictionConfig -ResourceGroupName $ResourceGroupName -Name $Name -SlotName $slotName
+    Foreach ($accessRestriction in $config.MainSiteAccessRestrictions)
+    {
+      $ruleName = $accessRestriction.RuleName
+      Write-Debug "Removing rule $ruleName"
+      Remove-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $Name -Name $accessRestriction.RuleName -SlotName $slotName
+    }
   }
+}
+
+function Add-AccessRestrictionToAppService([string] $ResourceGroupName, [string] $Name, [string] $IpFilterRuleName, [string] $IpAddress, [string] $Priority)
+{
+  Write-Information "Adding rule $IpFilterRuleName to allow api management service with ip $IpAddress and $Priority access to $Name"
+  Add-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $Name -Name $IpFilterRuleName -Priority $Priority -Action Allow -IpAddress $IpAddress
+  Update-AzWebAppAccessRestrictionConfig -Name $appService.Name -ResourceGroupName $ResourceGroupName -ScmSiteUseMainSiteRestrictionConfig
+
+  # Also add the access restriction to the staging slots
+  $allSlots = Get-AzWebAppSlot -ResourceGroupName $ResourceGroupName -Name $Name
+  Foreach ($slot in $allSlots)
+  {
+    $slotName = Get-SlotName -SlotName $slot.Name -AppServiceName $Name
+    Write-Information "Adding rule $IpFilterRuleName to allow api management service with ip $IpAddress and priority $Priority access to $Name slot $slotName"
+    Add-AzWebAppAccessRestrictionRule -ResourceGroupName $ResourceGroupName -WebAppName $Name -Slot $slotName -Name $IpFilterRuleName -Priority $Priority -Action Allow -IpAddress $IpAddress
+    Update-AzWebAppAccessRestrictionConfig -ResourceGroupName $ResourceGroupName -Name $Name -Slot $slotName -ScmSiteUseMainSiteRestrictionConfig
+  }
+}
+
+function Get-SlotName([string] $SlotName, [string] $AppServiceName)
+{
+  return $SlotName.Replace("$($AppServiceName)/", "")
 }
 
 function ReadJsonFile([string] $JsonFile)
